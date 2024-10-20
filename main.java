686d10bbb02955f563ddb692a30629ee393d459f
Yasas Aththanayaka <Aththanayakayasas@gmail.com>
2024-10-20 21:07:27 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,21 @@
+//Write a program to print numbers from 1 to 5 using for loop
+
+class main{
+	public static void main(String args[]){
+		int n =5;
+		
+		//for loop
+		for(int i=0; i<=n ;++i){
+			System.out.println(i);
+		}
+		System.out.println("\n ");
+		
+		//while loop
+		int i=0;
+		while(i<=n){
+			
+			System.out.println(i);
+			i++;
+		}
+	}
+}
